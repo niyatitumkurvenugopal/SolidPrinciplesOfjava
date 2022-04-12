f91ed2154af8fb7245ac376c905ca6f2e969ff16
106540 <niyati.venugopal@thoughtfocous.com>
2022-04-12 10:54:53 +0530

    Open and Close and Liskov principle has been added.

---
+++ src/main/java/com/niyati/solidprinciples/lsp/Bike.java
@@ -0,0 +1,14 @@
+package com.niyati.solidprinciples.lsp;
+
+public class Bike extends Vehicle{
+    public Bike(Vehicle vehicle){
+        super();
+    }
+    public String start(){
+        return "starting a bike.";
+    }
+
+    public String stop(){
+        return "stopping a bike.";
+    }
+}
---
+++ src/main/java/com/niyati/solidprinciples/lsp/package-info.java
@@ -0,0 +1,8 @@
+package com.niyati.solidprinciples.lsp;
+
+/**
+ * This principle states that we should be able to replace the base class objects with the subclasses objects
+ * and the code should work fine.
+ * Something unintended should not pop in subclass.
+ * Should not design the code in subclass which breaks the existence functionality.
+ */
